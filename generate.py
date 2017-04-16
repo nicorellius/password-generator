0baf405c038c0aa21c72e0ffa78b007785891c1d
Nick Vincent-Maloney <nicorellius@gmail.com>
2017-04-15 22:57:27 -0700

    Refactored password generator to create 5-word passphrases.

--- generate.py
+++ generate.py
@@ -26,14 +26,21 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-import os
+import logging
 import pprint
+
+from collections import defaultdict
 
 import click
 
 from urllib import request
+from urllib.error import HTTPError
 
 from rdoclient import RandomOrgClient
+
+import util
+
+logging.basicConfig(format='%(levelname)s %(message)s')
 
 # Try this first, and then use text file as failover
 WORDLIST_URL = 'https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt'
@@ -71,37 +78,59 @@ def generate_password(number_rolls, how_many=1, data_type='words',
     factor = 1
 
     if int(password_length) > 20:
+
         remainder = int(password_length) % 20
         factor = int(password_length) // 20
         tmp_length = 20
+
         print('temp length: {0}\nfactor: {1}\nremainder: {2}'.format(
-            tmp_length, 
+            tmp_length,
             factor,
             remainder)
         )
 
-    # TODO -- figure out how to add args for dice in generate password function
-    # TODO -- figure out how to add args for dice in generate password function
+        # TODO -- Append `remainder` worth characters onto password
 
     number_list = roll_dice()
-    # print(number_list)
-    # pprint.pprint(list(chunks(number_list, number_rolls)))
-    # pprint.pprint([number_list[i:i + 5] for i in range(1, 6)])
+    chunked_list = list(chunks(number_list, number_rolls))
+    pprint.pprint(chunked_list)
 
     if data_type == 'words':
+        try:
+            # Initialize list, dict, and variable
+            super_list = []
+            super_dict = {}
+            passphrase = ''
 
-        for line in request.urlopen(WORDLIST_URL):
-            # print(line)
-            pass
+            for line in request.urlopen(WORDLIST_URL):
+                l = line.decode()
+                d = {int(l.split('\t')[0]): l.split('\t')[1].strip('\n')}
+                super_list.append(d)
 
-        print("This will eventually print passphrase")
+            for k in set(k for d in super_list for k in d):
+                for d in super_list:
+                    if k in d:
+                        super_dict[k] = d[k]
+
+            for chunk in chunked_list:
+                n = int(''.join(map(str, chunk)))
+                passphrase += '{0} '.format(super_dict[n])
+
+            print(passphrase)
+
+        except HTTPError as e:
+            logging.error('[{0}] {1}'.format(util.get_timestamp(), e))
 
     elif data_type == 'numbers':
         chars = '1234567890'
+        print(''.join(roc.generate_strings(factor * how_many,
+                                           tmp_length,
+                                           chars)))
 
-    print(''.join(roc.generate_strings(factor * how_many,
-                                       tmp_length,
-                                       chars)))
+    else:
+        print(''.join(roc.generate_strings(factor * how_many,
+                                           tmp_length,
+                                           chars)))
 
 
 def roll_dice(number_rolls=5, number_dice=5, number_sides=6):
@@ -117,7 +146,7 @@ def roll_dice(number_rolls=5, number_dice=5, number_sides=6):
 
     roc = RandomOrgClient(API_KEY)
 
-    return roc.generate_integers(number_rolls * number_dice, 0, number_sides)
+    return roc.generate_integers(number_rolls * number_dice, 1, number_sides)
 
 
 def chunks(input_list, size):
