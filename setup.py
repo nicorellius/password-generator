5b35638e99f9d100f9fb4ed85baafa0e6943f884
Nick Vincent-Maloney <nicorellius@gmail.com>
2017-04-23 23:29:28 -0700

    Added metadata to setup.py for packaging.

--- setup.py
+++ setup.py
@@ -6,10 +6,21 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click',
+        'click',
+        'rdoclient',
+        'pytest',
     ],
     entry_points="""
         [console_scripts]
-        generate=generate:cli
+        generate=scripts.generate:cli
     """,
+
+    # Metadata for upload to PyPI
+    author='Nick Vincent-Maloney (nicorellius)',
+    author_email='nick@cistech.io',
+    description="""Dice Roll Optional, Mostly-Random Word, Number, 
+        and Mixed Character Password Generator""",
+    license='MIT',
+    keywords='password passphrase numbers words mixed eff dice roll',
+    url='http://cistech.io/projects/password-generator/',
 )
